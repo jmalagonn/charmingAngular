2a68fcd0c640c74aaed455ba9de33f3055101b88
Nicolas M <nmalagon@aplyca.com>
2019-06-18 22:32:24 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { HomeComponent } from './home/home/home.component';
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'desayunos', component: DesayunosComponent },
-  { path: '', redirectTo: '/charmingAngular/', pathMatch: 'full'},
 ];
 
 @NgModule({
